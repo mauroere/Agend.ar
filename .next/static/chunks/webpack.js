ed1cef804356c9a82f478f1f226fc5025321a8be
Mauro Rementeria <mauroere@gmail.com>
2026-01-06 09:50:40 -0300

    actualizaciones v2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d10de46e85a9dc65"; }
+/******/ 		__webpack_require__.h = function() { return "1076b76f93eeb042"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
